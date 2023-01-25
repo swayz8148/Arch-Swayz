42f24e6fe9060e993bcf572c05880377db0aa7a1
Swayz <113429324+swayz8148@users.noreply.github.com>
2023-01-25 17:46:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,12 +21,10 @@ pacman -Sy
 pacman -S pacman-contrib
 rankmirrors -n 10 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist
 
-pacstrap -i /mnt base base-devel linux linux-lts linux-headers linux-firmware intel-ucode
-sudo nano vim git neofetch networkmanager dhcpcd pulseaudio
+pacstrap -i /mnt base base-devel linux linux-lts linux-headers linux-firmware intel-ucode sudo nano vim git neofetch networkmanager dhcpcd pulseaudio
 
 genfstab -U /mnt > /mnt/etc/fstab
-arch-chroot /mnt
-passwd
+arch-chroot /mnt passwd
 
 while true; do
 
@@ -47,32 +45,32 @@ esac
 
 done
 
-useradd -m $username
-passwd $username
-usermod -aG wheel,storage,power $username
+arch-chroot /mnt useradd -m $username
+arch-chroot /mnt passwd $username
+arch-chroot /mnt usermod -aG wheel,storage,power $username
 
-EDITOR=nano visudo
+arch-chroot /mnt EDITOR=nano visudo
 
-nano /etc/locale.gen
-locale-gen
-echo LANG=en_GB.UTF-8 > /etc/locale.conf
-export LANG=_GB.UTF-8
+arch-chroot /mnt nano /etc/locale.gen
+arch-chroot /mnt locale-gen
+arch-chroot /mnt echo LANG=en_GB.UTF-8 > /etc/locale.conf
+arch-chroot /mnt export LANG=_GB.UTF-8
 
-echo arch-swayz > /etc/hostname
+arch-chroot /mnt echo arch-swayz > /etc/hostname
 
-nano /etc/hosts
+arch-chroot /mnt nano /etc/hosts
 
-ln -sf /usr/share/zoneinfo/Europe/london /etc/localtime
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/london /etc/localtime
 
-mkdir /boot/efi
-mount /dev/sda1 /boot/efi/
-pacman -S grub efibootmgr dosfstools mtools
-nano /etc/default/grub
-pacman -S os-prober
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
-systemctl enable dhcpcd.service
-systemctl enable NetworkManager.service
+arch-chroot /mnt mkdir /boot/efi
+arch-chroot /mnt mount /dev/sda1 /boot/efi/
+arch-chroot /mnt pacman -S grub efibootmgr dosfstools mtools
+arch-chroot /mnt nano /etc/default/grub
+arch-chroot /mnt pacman -S os-prober
+arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt systemctl enable dhcpcd.service
+arch-chroot /mnt systemctl enable NetworkManager.service
 exit
 
 echo Install has finished
